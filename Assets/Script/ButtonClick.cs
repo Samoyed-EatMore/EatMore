065bb1c65ee3b8445a7ff6697869642b4c532fc9
wangxin <14302010064@fudan.edu.cn>
2017-01-24 23:02:32 +0800

    The rough model of this game.

---
+++ Assets/Script/ButtonClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// Help Button click
+	public void  clickForHelp () {
+	    SceneManager.LoadSceneAsync("help_scene");
+    }
+
+    // Start Button click
+	public void clickForStart () { 
+	    SceneManager.LoadSceneAsync("game_scene");
+    }
+
+    // Return Button click
+	public void clickForReturn () { 
+	    SceneManager.LoadSceneAsync("start_scene");
+    }
+}
